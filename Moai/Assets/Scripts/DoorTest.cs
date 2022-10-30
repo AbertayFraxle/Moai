a2b8ede6dd9ca075f024e3eff65dfef9b21ae226
JULIAN BANKS <2203401@uad.ac.uk>
2022-10-30 12:44:41 +0000

    Cell
    Added door

--- Moai/Assets/Scripts/DoorTest.cs
+++ Moai/Assets/Scripts/DoorTest.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class DoorTest : MonoBehaviour
 {
+    public bool isOpen;
     [SerializeField] Transform pivot;
+    [SerializeField] float openTime;
     Vector3 position;
-    private void Start()
+
+    public void ChangeDoorState()
     {
-        StartCoroutine(Rotate());
+        StopAllCoroutines();
+        if (isOpen)
+        {
+            isOpen = false;
+            StartCoroutine(CloseDoor());
+        }
+        else
+        {
+            isOpen = true;
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    private IEnumerator OpenDoor()
+    {
+        float timer = 0;
+        while (timer < openTime)
+        {
+            pivot.transform.localRotation = Quaternion.Lerp(pivot.transform.localRotation, Quaternion.Euler(0, -90, 0), timer / openTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator CloseDoor()
+    {
+        float timer = 0;
+        while (timer < openTime)
+        {
+            pivot.transform.localRotation = Quaternion.Lerp(pivot.transform.localRotation, Quaternion.identity, timer / openTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private IEnumerator Rotate()
