9eda8df14d12d5198ec3b3bae08edce82ee3cc74
AbertayFraxle <64607955+profraxle@users.noreply.github.com>
2022-11-13 09:05:23 +0000

    fixed some bugs

--- Moai/Assets/gomenu.cs
+++ Moai/Assets/gomenu.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class gomenu : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void END()
     {
         SceneManager.LoadScene(0, LoadSceneMode.Single);
